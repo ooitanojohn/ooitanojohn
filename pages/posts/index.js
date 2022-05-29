8d990a73d39387d925fbd1b04c8d775141f9ed7d
ooitanojohn <qmkh733310@gmail.com>
2022-05-29 19:28:10 +0900

    [add] git-flow形式でblog作り進めていく

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,8 +3,9 @@ import Link from 'next/link'
 export default function Index(params) {
   return (
     <>
-      <h1>index.js</h1>
-      <Link href="/posts/route"><a>route.jsへ</a></Link>
+      <h1>テスト用のページだよ index.js</h1>
+      <a href="http://ooitanojohn.wp.xdomain.jp/">外部リンクを貼るときはaタグのみ</a>
+      <Link href="/posts/route"><a className='a_test_class'>route.jsへ</a></Link>
       <Link href="/posts/api"><a>api.jsへ</a></Link>
       <Link href="/"><a>Index.jsへ</a></Link>
     </>
