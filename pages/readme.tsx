ae7b3249330cbafd3c3cbfb5d7a5b12188e7c391
ooitanojohn <qmkh733310@gmail.com>
2022-06-14 15:24:41 +0900

    [fix] importのpath修正

--- pages/readme.tsx
+++ pages/readme.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../../components/header'
-import Footer from '../../components/footer'
+import Header from '../components/header'
+import Footer from '../components/footer'
 import { GetStaticProps } from 'next'
 import { getGitProfileData } from '../../lib/git'
 
