e4779e91bab477c2db43f319e265d4142101a2dc
ooitanojohn <qmkh733310@gmail.com>
2022-10-05 00:09:23 +0900

    [add] markdown.css 追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 // 毎回ページで読み込む設定 globalcssを読み込んだりする
 import "../styles/destyle.css"
+import "../styles/retro.css"
+//import "../styles/sakura_earthly.css"
 import { AppProps} from 'next/app'
 
 function MyApp({ Component, pageProps }:AppProps) {
